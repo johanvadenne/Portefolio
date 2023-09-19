99a1a5894c5263b8520d13b232180abf17184688
wolffire18 <johanvadenne@gmail.com>
2023-09-19 17:46:20 +0200

    js

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 let elementTitre = document.getElementById("titreJohanVadenne");
-let elementsLien = document.getElementById("contenantLien");
+let contenantElementsLien = document.getElementById("contenantLien");
+let menuJohan = document.getElementsByClassName("menuJohan");
+let elementsLien = document.querySelectorAll(".lien");
+
+const elementAccueil = [
+    { id: "qui_je_suis", color: "linear-gradient(135deg, rgb(49, 21, 177), rgb(188, 55, 206))" },
+    { id: "etude_Entreprise", color: "linear-gradient(135deg, rgb(49, 21, 177), rgb(188, 55, 206))" },
+    { id: "projet", color: "linear-gradient(135deg, rgb(49, 21, 177), rgb(188, 55, 206))" },
+    { id: "veille", color: "linear-gradient(135deg, rgb(49, 21, 177), rgb(188, 55, 206))" },
+    { id: "cv", color: "linear-gradient(135deg, rgb(49, 21, 177), rgb(188, 55, 206))" },
+];
 
 function texteMenu() {
-    endurscorClignotement(function() {ecritureMenu()});
+    endurscorClignotement(function() { ecritureMenu() });
 }
 
 function endurscorClignotement(callBack = "") {
-    
+
     let indClignotement = 0;
     let visible = true;
     let texte = elementTitre.innerHTML;
 
     let interval = setInterval(function() {
         if (visible) {
-            elementTitre.innerHTML = texte+"_"
+            elementTitre.innerHTML = texte + "_"
             visible = !visible;
-        }
-        else {
-            elementTitre.innerHTML = texte+" "
+        } else {
+            elementTitre.innerHTML = texte + " "
             visible = !visible;
         }
 
@@ -27,14 +36,14 @@ function endurscorClignotement(callBack = "") {
             try {
                 callBack();
             } catch (error) {
-                
+
             }
         }
     }, 300)
 }
 
 function ecritureMenu() {
-    
+
     const titre = "Johan Vadenne ";
     let ind = 0;
     let maxInd = titre.length - 1;
@@ -43,25 +52,39 @@ function ecritureMenu() {
 
     titreAfficher = "_";
     elementTitre.innerHTML = titreAfficher
-    
+
     let interval = setInterval(function() {
 
         titreEnCours += titre[ind]
-        titreAfficher = titreEnCours+"_";
+        titreAfficher = titreEnCours + "_";
         elementTitre.innerHTML = titreAfficher;
 
         ind++;
         if (ind > maxInd) {
             clearInterval(interval);
             elementTitre.innerHTML = titre;
-            elementsLien.style.transition = "opacity 0.3s";
+            contenantElementsLien.style.transition = "opacity 0.3s";
             let interval2 = setInterval(function() {
-                elementsLien.style.opacity = 1;
+                contenantElementsLien.style.opacity = 1;
                 clearInterval(interval2);
             }, 500)
         }
 
     }, 100)
+}
+
+for (let elementLien of elementsLien) {
+
+    elementLien.addEventListener("click", function() {
+        for (let elementmenuJohan of menuJohan) {
+            elementmenuJohan.style.opacity = 0;
+        }
+    });
+
+    elementLien.addEventListener("mouseover", function() {
+        let elementSurvoler = elementLien.id;
+        elementmenuJohan.style.background = elementAccueil[1].color;
+    });
 }
 
 texteMenu();
